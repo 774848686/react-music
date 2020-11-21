180865016c150ab83b0cadfb1c8c94ef1db04fe7
774848686 <774848686@qq.com>
2020-11-21 17:01:37 +0800

    add:歌手页面

--- src/baseUI/Scroll/index.js
+++ src/baseUI/Scroll/index.js
@@ -2,19 +2,38 @@ import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle } f
 import PropTypes from "prop-types"
 import BScroll from "better-scroll"
 import styled from 'styled-components';
+import Loading from '../loading';
+import LoadingV2 from '../loading-v2/index';
 
 const ScrollContainer = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
 `
+const PullUpLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  bottom: 5px;
+  width: 60px;
+  height: 60px;
+  margin: auto;
+  z-index: 100;
+`;
+
+export const PullDownLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  top: 0px;
+  height: 30px;
+  margin: auto;
+  z-index: 100;
+`;
 const Scroll = forwardRef((props, ref) => {
     // 编写组件内容
     //better-scroll 实例对象
     const [bScroll, setBScroll] = useState();
     //current 指向初始化 bs 实例需要的 DOM 元素 
     const scrollContaninerRef = useRef();
-    console.log(ref)
     const { direction, click, refresh, pullUpLoading, pullDownLoading, bounceTop, bounceBottom } = props;
     const { pullUp, pullDown, onScroll } = props;
     useEffect(() => {
@@ -91,15 +110,21 @@ const Scroll = forwardRef((props, ref) => {
             }
         }
     }));
+    const PullUpdisplayStyle = pullUpLoading ? {display: ""} : { display:"none" };
+const PullDowndisplayStyle = pullDownLoading ? { display: ""} : { display:"none" };
     return (
         <ScrollContainer ref={scrollContaninerRef}>
-            {props.children}
-        </ScrollContainer>
+        {props.children}
+        {/* 滑到底部加载动画 */}
+        <PullUpLoading style={ PullUpdisplayStyle }><Loading></Loading></PullUpLoading>
+        {/* 顶部下拉刷新动画 */}
+        <PullDownLoading style={ PullDowndisplayStyle }><LoadingV2></LoadingV2></PullDownLoading>
+      </ScrollContainer>
     )
 });
 Scroll.propTypes = {
     direction: PropTypes.oneOf(['vertical', 'horizental']),// 滚动的方向
-    click: true,// 是否支持点击
+    click: PropTypes.bool,// 是否支持点击
     refresh: PropTypes.bool,// 是否刷新
     onScroll: PropTypes.func,// 滑动触发的回调函数
     pullUp: PropTypes.func,// 上拉加载逻辑
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,27 +1,33 @@
 import React from 'react';
+//components/list.js
+// 引入
+import LazyLoad from "react-lazyload";
 import { getCount } from "../../api/utils";
-import { 
+import {
   ListWrapper,
   ListItem,
   List
 } from './style';
 
-function RecommendList (props) {
+function RecommendList(props) {
   return (
     <ListWrapper>
       <h1 className="title"> 推荐歌单 </h1>
       <List>
         {
-          props.recommendList.map ((item, index) => {
+          props.recommendList.map((item, index) => {
             return (
               <ListItem key={item.id + index}>
                 <div className="img_wrapper">
                   <div className="decorate"></div>
-                    {/* 加此参数可以减小请求的图片资源大小 */}
-                    <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music"/>
+                  {/* 加此参数可以减小请求的图片资源大小  img 标签外部包裹一层 LazyLoad */}
+
+                  <LazyLoad placeholder={<img width="100%" height="100%" src={require('./music.png')} alt="music" />}>
+                    <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music" />
+                  </LazyLoad>
                   <div className="play_count">
                     <i className="iconfont play">&#xe885;</i>
-                    <span className="count">{getCount (item.playCount)}</span>
+                    <span className="count">{getCount(item.playCount)}</span>
                   </div>
                 </div>
                 <div className="desc">{item.name}</div>
@@ -32,6 +38,6 @@ function RecommendList (props) {
       </List>
     </ListWrapper>
   );
-  }
- 
-export default React.memo (RecommendList);
+}
+
+export default React.memo(RecommendList);
